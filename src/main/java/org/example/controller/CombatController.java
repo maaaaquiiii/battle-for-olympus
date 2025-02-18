5ae7d10052c23f614e5f8c1900c311a39677b3a2
maaaaquiiii <maqui.plaza02@gmail.com>
2025-02-18 12:27:55 +0100

    fiveteenth commit

--- src/main/java/org/example/controller/CombatController.java
+++ src/main/java/org/example/controller/CombatController.java
@@ -11,51 +11,64 @@ public class CombatController {
     private final PotionController potionController;
     private final WeaponController weaponController;
     private final Random random;
+    private boolean playerRandomMood;
 
     public CombatController() {
         this.characterController = new CharacterController();
         this.potionController = new PotionController();
         this.weaponController = new WeaponController();
         this.random = new Random();
+
     }
 
     public void initiateCombat(Character character1, Character character2) {
         boolean isCombatOver = false;
         while (!isCombatOver) {
-            character1Turn(character1, character2);
+            characterTurn(character1, character2, false);
             if(!character2.isAlive()) {
                 isCombatOver = true;
             }
-            characterTurn(character2, character1);
+            boolean player2Option = getPlayerOption(character2);
+            characterTurn(character2, character1, player2Option);
         }
     }
 
-    private void character1Turn(Character character1, Character character2) {
-        int action = getManualAction(character1);
-        switch (action) {
-            case 1 -> attack(character1, character2);
-            case 2 -> {
-                int index = getManualAction(character1);
-                useManualPotion(character1, index);
-            }
-            case 3 -> {
-                int index = getManualAction(character1);
-                equipManualWeapon(character1, index);
+    private void characterTurn(Character character1, Character character2, boolean playerRandomMood) {
+        if(!playerRandomMood) {
+            int action = getManualAction(character1);
+            switch (action) {
+                case 1 -> attack(character1, character2);
+                case 2 -> {
+                    int index = getManualPotionIndex(character1);
+                    useManualPotion(character1, index);
+                }
+                case 3 -> {
+                    int index = getManualWeaponIndex(character1);
+                    equipManualWeapon(character1, index);
+                }
             }
         }
-    }
-
-
-    private void characterTurn(Character attacker, Character defender) {
         int action = getRandomAction();
         switch (action) {
-            case 2 -> character2UsePotion(attacker);
-            case 3 -> equipWeaponRandomly(attacker);
-            default -> attack(attacker, defender);
+            case 2 -> character2UsePotion(character1);
+            case 3 -> equipWeaponRandomly(character1);
+            default -> attack(character1, character2);
         }
     }
 
     private int getManualAction(Character character) {
+        throw new UnsupportedOperationException("Manual input managed by MenuView");
+    }
+
+    private int getManualPotionIndex(Character character) {
+        throw new UnsupportedOperationException("Manual input managed by MenuView");
+    }
+
+    private int getManualWeaponIndex(Character character) {
+        throw new UnsupportedOperationException("Manual input managed by MenuView");
+    }
+
+    private boolean getPlayerOption(Character character) {
         throw new UnsupportedOperationException("Manual input managed by MenuView");
     }
 
